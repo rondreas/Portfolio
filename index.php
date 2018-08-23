e338d1e93c87a340f36a7ea84e4274f1ffbc6bfc
Andreas Rånman <andreas.ranman@gmail.com>
2018-08-23 14:51:41 +0200

    Conformed my IDs to wordpress example
    Changed the name of my class as I noticed I had it the otherway around from the themes I've been looking at. Also added it to the wp_nav_menu call so it's less dependent on what I name the menu.

--- index.php
+++ index.php
@@ -5,6 +5,7 @@
 <? if ( has_nav_menu( 'front' ) ) : ?>
   <?php wp_nav_menu( array(
       'theme_location' => 'front',
+      'menu_id' => 'front-menu',
       'walker' => new Custom_Walker()
   ) ); ?>
 <?php endif; ?>
