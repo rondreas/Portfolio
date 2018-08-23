4ff8fcaf1cb91bddcb8402e23dd72f0bdc68ed36
Andreas Rånman <andreas.ranman@gmail.com>
2018-08-23 14:47:53 +0200

    Altered stylesheet & removed redundant class
    Did some major changes in names in the stylesheet.
    
    Also moved away from styling ul and li elements, instead calling the IDs or Classes I want to target so the adminbar doesn't break.

--- index.php
+++ index.php
@@ -5,7 +5,6 @@
 <? if ( has_nav_menu( 'front' ) ) : ?>
   <?php wp_nav_menu( array(
       'theme_location' => 'front',
-      'menu_class' => 'splash-nav',
       'walker' => new Custom_Walker()
   ) ); ?>
 <?php endif; ?>
