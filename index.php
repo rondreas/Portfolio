09d70f731ab4fa7c465fbe818db1857ac177d7d0
andreas rånman <andreas.ranman@gmail.com>
2018-08-24 17:46:21 +0200

    Added else state which will bring an under construction section up instead of front nav. Also added styling for under construction section.

--- index.php
+++ index.php
@@ -2,12 +2,19 @@
 
 <div class="wrap">
 
-<? if ( has_nav_menu( 'front' ) ) : ?>
+<?php if ( has_nav_menu( 'front' ) ) : ?>
   <?php wp_nav_menu( array(
       'theme_location' => 'front',
       'menu_id' => 'front-menu',
       'walker' => new Custom_Walker()
   ) ); ?>
+<?php else : ?>
+  <section class='under-construction'>
+    <div>
+      <h1>Coming Soon</h1>
+      <p>Site is currently under construction</p>
+    </div>
+  </section><!-- .under-construction -->
 <?php endif; ?>
 
 </div><!-- .wrap -->
