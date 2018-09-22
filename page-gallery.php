32f8b2c8f1a07ab8dc6c3e92d4169f579765243d
Andreas Rånman <andreas.ranman@gmail.com>
2018-09-22 17:19:09 +0200

    added page for gallery

---
+++ page-gallery.php
@@ -0,0 +1,27 @@
+<?php get_header(); ?>
+
+<div class="wrap">
+  <div id="primary" class="content-area">
+    <div id="main" class="site-main page-gallery">
+
+    <?php
+    /* Start the Loop */
+    while ( have_posts() ) : the_post(); ?>
+
+      <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+
+        <div class="entry-content">
+          <?php
+            the_content();
+          ?>
+        </div>
+
+      </article>
+
+    <?php endwhile; // End of the loop. ?>
+
+    </div> <!-- #primary -->
+  </div> <!-- .wrap -->
+</div> <!-- .wrap -->
+
+<?php get_footer(); ?>
