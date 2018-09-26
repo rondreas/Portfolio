10da183a03c45fd3af4c68d563d03ff56f6d1b64
andreas rånman <andreas.ranman@gmail.com>
2018-09-26 15:59:42 +0200

    Updated styling to have a better scaling font.

--- header.php
+++ header.php
@@ -4,6 +4,7 @@
 <head>
 
 <meta charset="<?php bloginfo( 'charset' ); ?>">
+<meta name="viewport" content="width=device-width,initial-scale=1">
 <link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>">
 
 <?php wp_head(); ?>
